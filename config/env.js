f1656f3dbda50eac92749156f6b669fc0165da10
yangc <yangc@usoftchina.com>
2018-11-29 09:57:41 +0800

    初始化项目

---
+++ config/env.js
@@ -0,0 +1,13 @@
+const isProdMode = Object.is(process.env.NODE_ENV, 'production')
+
+const baseUrl = isProdMode ? 'https://finrest.usoftchina.com' : 'https://finrest.uuzcc.cn'
+
+const authorityUrl = isProdMode ? 'https://securityrest.usoftchina.com' : 'https://securityrest.uuzcc.cn'
+
+const basicAdminUrl = isProdMode ? 'https://basicadmin.usoftchina.com' : 'https://basicadmin.uuzcc.cn'
+
+module.exports = {
+    baseUrl: baseUrl,
+    authorityUrl: authorityUrl,
+    basicAdminUrl: basicAdminUrl
+}
--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,6 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
   }
 })
