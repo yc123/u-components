ecd76fbcae02a149fa1227886a3e798a3438c251
yangc <yangc@usoftchina.com>
2018-11-29 10:01:34 +0800

    初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+      component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
     }
   ]
 })
